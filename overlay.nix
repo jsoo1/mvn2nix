72a673000128a1ccaca7258c1cda70f8fe2bab38
John Soo <johh.soo@arista.com>
2024-07-16 13:48:01 -0600

    overlay: Remove jdk overrides

--- overlay.nix
+++ overlay.nix
@@ -1,12 +1,7 @@
 self: super: {
-  mvn2nix = self.callPackage ./derivation.nix {
-    jdk = super.jdk16_headless;
-  };
+  mvn2nix = self.callPackage ./derivation.nix { };
 
-  mvn2nix-bootstrap = self.callPackage ./derivation.nix {
-    bootstrap = true;
-    jdk = super.jdk16_headless;
-  };
+  mvn2nix-bootstrap = self.callPackage ./derivation.nix { bootstrap = true; };
 
   buildMavenRepository =
     (self.callPackage ./maven.nix { }).buildMavenRepository;
