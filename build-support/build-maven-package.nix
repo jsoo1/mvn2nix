57bf31d3d8dc008bc0ab5474a46a9b250e2fa6cd
John Soo <johh.soo@arista.com>
2024-08-09 03:48:05 -0600

    buildMavenPackage: Add classpath patching mechanism

--- build-support/build-maven-package.nix
+++ build-support/build-maven-package.nix
@@ -3,6 +3,7 @@
 , jdk
 , mvn2nix
 , maven
+, writeText
 }:
 
 /*
@@ -17,17 +18,35 @@
 , artifactId
 , classifier ? null
 , dependencies
+  # Whether or not to add the runtime dependencies of the
+  # package to the manifesh Class-Path field.
+, patchClasspath ? false
 , ...
 }@args:
 
 let
+  # TODO(jsoo1): This classpath patching/nix-support linking should be a
+  # stdenv hook
+  classpath = mvn2nix.lib.mkRuntimeClasspath dependencies;
+
+  # Make a file to link in $out/nix-support for nix to scan
+  classpathFile = writeText "${name}-${version}-classpath"
+    (lib.concatStringsSep " " classpath);
+
+  manifest = writeText "${name}-${version}-MANIFEST.MF"
+    (mvn2nix.lib.mkManifestClassPath classpath);
+
   repository = mvn2nix.mkMavenRepository dependencies;
 in
 
 stdenv.mkDerivation ({
-  inherit name version src repository;
+  inherit name version src repository patchClasspath;
 
-  passthru.dependencies = dependencies;
+  passthru = {
+    dependencies = dependencies;
+  } // lib.optionalAttrs patchClasspath {
+    inherit classpath manifest;
+  };
 
   nativeBuildInputs = [ jdk maven ] ++ (args.nativeBuildInputs or [ ]);
 
@@ -38,9 +57,25 @@ stdenv.mkDerivation ({
   '';
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/share/java
 
     cp target/${name}-${version}.jar $out/share/java
+  '' + (lib.optionalString patchClasspath ''
+    # Date is to limited to DOS date range:
+    # https://bugs.openjdk.org/browse/JDK-8184940
+    # FIXME(jsoo1): This style should be dependent on java version
+    # jar --update --date=1980-01-01T00:00:02Z --file=$filename --manifest=${manifest}
+
+     jar -ufm $out/share/java ${manifest}
+
+     mkdir -p $out/nix-support
+
+     ln -s ${classpathFile} $out/nix-support/classpath
+  '') + ''
+
+    runHook postInstall
   '';
 
 } // lib.removeAttrs args [ "dependencies" ])
